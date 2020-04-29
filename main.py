30eca9c485e9fc6748d7ef240aba1def5b2d3469
Bekhruz Niyazov <50236015+BekhruzSNiyazov@users.noreply.github.com>
2020-04-30 01:29:46 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import smtplib
+
+server = input("Which server are you using (gmail, outlook): ")
+_from = input("Type your email address: ")
+password = input("Type your password: ")
+to = input("Send to: ")
+subject = input("Subject: ")
+body = input("Body: ")
+message = f"""\
+Subject: {subject}
+
+{body}"""
+server = smtplib.SMTP(f"smtp.{server}.com", 587)
+server.starttls()
+server.login(_from, password)
+server.sendmail(_from, to, message)
